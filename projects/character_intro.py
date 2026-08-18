c5065e67214f58867274dc7524649f1c8259c078
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2026-08-18 09:50:12 -0600

    finished character intro

---
+++ projects/character_intro.py
@@ -0,0 +1,42 @@
+# Addie Erickson | Character Intro
+import time
+
+# variables
+name = "Vally"
+age_venus = "37"
+age_earth = "23"
+job = "Shuttle Repair Crew"
+where_from = "Venus"
+
+
+print("INTERVIEW WITH SPACE COLONIST")
+time.sleep(1)
+print("INTERVIEWER: Welcome, this is exciting, you are one of the few people living on a space colony?")
+time.sleep(2)
+print("COLONIST: Correct, although the crew and I are not currently settled in yet, we have just arrived.")
+time.sleep(2)
+print("INTERVIEWER: Perfect, we'll just ask you a few questions, we won't take much time. First where is that space colony located?")
+time.sleep(4)
+print("COLONIST: Venus")
+time.sleep(1)
+print("INTERVIEWER: Ooh, thats an intersting place to live! What is your name?")
+time.sleep(2)
+print("VENUS COLONIST: " + name)
+time.sleep(1)
+print("INTERVIEWER: Very nice to meet you " + name + ". Now, how old are you?")
+time.sleep(1.5)
+print("VALLY: I am " + age_venus)
+time.sleep(1)
+print("INTERVIEWER: What would that be in Earth years?")
+time.sleep(1)
+print("VALLY: " + age_earth)
+time.sleep(1)
+print("INTERVIEWER: And your job currently?")
+time.sleep(1)
+print("VALLY: " + job)
+time.sleep(1)
+print("INTERVIEWER: Thank you for coming to this interview, we will do another catch up after you are settled in.")
+time.sleep(3)
+print("VALLY: Thank you for inviting me, can't wait for the next.")
+time.sleep(2)
+print("END OF INTERVIEW")
