389c2197a1f0940e75495dae6bd29c8fb2e44a7c
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2026-08-24 09:10:27 -0600

    done

---
+++ projects/debugging.py
@@ -0,0 +1,7 @@
+# Addie Erickson | Fix the Code
+
+name = "Alex"
+age = 16
+
+print("My name is " + name + " and I am " + str(age) + " years old.") # parenthesis did not close, fixed
+                     # ^ variable "name" was not spelled correctly within print statement, fixed
