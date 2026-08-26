306dbfd53d5c43c9d5c5aa72f41554fc94c57745
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2026-08-26 09:24:09 -0600

    hellow

---
+++ pesudocode.py
@@ -0,0 +1,31 @@
+# Addie Erickson | Pseudocode
+
+"""questions to ask (input)"""
+    # fav color | color = input("What is your favorite color?")
+    
+    # name | name = input("What is your name?")
+    
+    # age | age = input("How old are you??")
+    
+    # hobby | hobby = input("Do you have a hobby that you like to do in your free time?")
+    
+    # planet of residence | planet = input("What planet do you live on? Prefreably in our known solar system, intergalactic and interstellar residencies can get complex.")
+
+
+"""i'm not sure if i do it will include this: if you type in "randomize" to one of the inputs it will give you a random generated output"""
+
+"""I might also do where there will be a responce to each input using if/else, ex:"""
+    # color = input("What is your favorite color?")
+    # if "green" in color or "Green" in color:
+        # print("Really? Thats mine too!")
+
+
+"""variables (these are just variables, also listed above)"""
+    # color 
+    # name
+    # age
+    # hobby
+    # planet
+
+"""final introduction (output)"""
+    # "Very nice, so your ", name, " and you are ", age, " years old and you like ", color, " and ", hobby, ", right? If thats not who you are then theres nothing I can do, good luck."
