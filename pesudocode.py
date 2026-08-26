e62d003b8eceb6976576520f7b66d6f68bc1d258
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2026-08-26 09:33:19 -0600

    hehe

--- pesudocode.py
+++ pesudocode.py
@@ -1,26 +1,30 @@
 # Addie Erickson | Pseudocode
 
 """questions to ask (input)"""
-    # fav color | color = input("What is your favorite color?")
+    # fav color | color = input("What is your favorite color?").lower()
     
-    # name | name = input("What is your name?")
+    # name | name = input("What is your name?").lower()
     
-    # age | age = input("How old are you??")
+    # age | age = input("How old are you??").lower()
     
-    # hobby | hobby = input("Do you have a hobby that you like to do in your free time?")
+    # hobby | hobby = input("Do you have a hobby that you like to do in your free time?").lower()
     
-    # planet of residence | planet = input("What planet do you live on? Prefreably in our known solar system, intergalactic and interstellar residencies can get complex.")
+    # planet of residence | planet = input("What planet do you live on? Prefreably in our known solar system, intergalactic and interstellar residencies can get complex.").lower()
 
 
 """i'm not sure if i do it will include this: if you type in "randomize" to one of the inputs it will give you a random generated output"""
 
-"""I might also do where there will be a responce to each input using if/else, ex:"""
-    # color = input("What is your favorite color?")
-    # if "green" in color or "Green" in color:
-        # print("Really? Thats mine too!")
+"""I will also do where there will be a responce to each input using if/else, ex: """
+    # color = input("What is your favorite color?").lower()
+    # if "green" in color or "orange" in color:
+        # print(color, " is mine too!")
+    # elif "pink" in color:
+        # print("Really? ", color, "?? Not even a different shade?")
+    # else:
+        # print(color, " is a nice color.")
 
 
-"""variables (these are just variables, also listed above)"""
+"""variables (these are just the variables, also listed above)"""
     # color 
     # name
     # age
