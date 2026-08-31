09994f4fa34b2ef977eef6ad5c9e051e2570a866
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2026-08-31 09:52:36 -0600

    not done

---
+++ notes/integers_floats.py
@@ -0,0 +1,18 @@
+# Addie Erickson | Integers & Floats Notes
+
+# integer
+num = 75 # <- just a number
+
+# float
+pi = 3.1415926 # <- just write number
+
+print(7*4)
+print(5/2)
+print(5//2) # <- intieger division
+
+# modulo % (gives remainder that is left over)
+print(5%2)
+print(10%3)
+print(15%5)
+
+# pemmdas (modulo included)
---
+++ projects/average_grade.py
@@ -0,0 +1,12 @@
+# Addie Erickson | Average Grade
+
+# 7 diff inputs
+# output rouded to 2 decimals
+# code works
+# add loop
+
+print("Enter all classes that you have.") # beginning instructions
+
+grade_one_class = input("Enter class: ") # <- inputs (enter class)
+
+# add inputs for number grade
