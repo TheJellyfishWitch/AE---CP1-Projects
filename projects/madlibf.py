9c18d99af3898ba2a582dd0268cfc021fef0ac8a
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2026-09-01 18:21:29 +0000

    not done

---
+++ projects/madlibf.py
@@ -0,0 +1,20 @@
+# Addie Erickson | Madlib
+
+#Charlie and Itchy were in the ROOM of PLACE. They were having a ADVERB competition. A cook off! Sasha was the judge! “Ok, you will both be making FOOD with cheese!” Charlie and Itchy both got to work! Charlie PAST TENSE VERB a pan and began to make the FOOD. Itchy PAST TENSE VERB 2 a pot and started to make the SAME FOOD. Charlie and Itchy started by adding COUNTRY FORM OF MEAT, some VEGGIE, and cheese! Itchy added some SPICE. After NUMBER hour (s), the time was up! Sasha tried Charlie’s dish first, she PAST TENSE VERB 3 and said it was ADJECTIVE. Then she tried Itchy’s dish. She PAST TENSE VERB 4 with a smile! “WORD Itchy! You win” she Said
+
+name = input("Enter your name: ")
+other_name = input("Enter a name: ")
+room = input("Enter a room: ")
+place = input("Enter a place: ")
+adverb = input("Enter a adverb: ")
+place = input("Enter a place: ")
+place = input("Enter a place: ")
+place = input("Enter a place: ")
+place = input("Enter a place: ")
+place = input("Enter a place: ")
+place = input("Enter a place: ")
+place = input("Enter a place: ")
+place = input("Enter a place: ")
+place = input("Enter a place: ")
+place = input("Enter a place: ")
+place = input("Enter a place: ")
