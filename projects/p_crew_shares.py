9640fcfe41d633ec37da44ba902319d95a7b3e45
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2026-09-18 09:25:46 -0600

    done?

---
+++ projects/p_crew_shares.py
@@ -0,0 +1,22 @@
+# AE | P1 | Crew Shares Pseudocode
+
+# LIST holds all names of pirates
+# GET number of pirates from user
+# WHILE TRUE
+    # IF number of pirates <= number of names
+        # GET name of pirate, add to list
+        # CALCULATE number of names + 1
+    # ElSE
+        # BREAK
+
+# CALCULATE add 2 to number of pirates for total including Yondu and Quill
+# RANDOM units between 500, 5000, whole number
+# CALCULATE Yondu's share: 13% of units, round
+# CALCULATE Peter's share: 11% of left over units, round
+# CALCULATE Crew's share: left over units into how many pirates were entered in the beginning, round
+
+# OUTPUT # of pirates including Yondu and Quill
+# OUTPUT random # of units
+# OUTPUT Yondu's share
+# OUTPUT Peter's share
+# OUTPUT all names of crew including individual share
