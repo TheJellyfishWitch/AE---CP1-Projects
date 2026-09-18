e7e87f760d36d39f4c68252e4b66c955a38251d8
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2026-09-18 09:50:33 -0600

    changed

--- projects/crew_shares.py
+++ projects/crew_shares.py
@@ -3,11 +3,23 @@
 import random
 import time
 
+name_pirates = []
+
 while True:
     try:
         number_pirates = int(input("How many pirates are there? "))
     except:
         print("That is not a number, try again")
+    else:
+        break
+
+number = 0
+
+while True:
+    if number_pirates <= number:
+        for i in range(number_pirates):
+            name_pirates = input(f"What is the name of pirate #{i+1}? ")
+        number += 1
     else:
         break
 
--- projects/p_crew_shares.py
+++ projects/p_crew_shares.py
@@ -13,7 +13,7 @@
 # RANDOM units between 500, 5000, whole number
 # CALCULATE Yondu's share: 13% of units, round
 # CALCULATE Peter's share: 11% of left over units, round
-# CALCULATE Crew's share: left over units into how many pirates were entered in the beginning, round
+# CALCULATE Crew's share: left over units into how many pirates were entered in the beginning not the number including Yondu and Quill, round
 
 # OUTPUT # of pirates including Yondu and Quill
 # OUTPUT random # of units
