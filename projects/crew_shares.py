0eb70e4ec64f0f632945cc1802a36eb410419342
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2026-09-18 09:46:40 -0600

    done

---
+++ projects/crew_shares.py
@@ -0,0 +1,33 @@
+# AE | P1 | Crew Shares
+
+import random
+import time
+
+while True:
+    try:
+        number_pirates = int(input("How many pirates are there? "))
+    except:
+        print("That is not a number, try again")
+    else:
+        break
+
+number_pirates_w_other = number_pirates + 2 
+
+units = random.randint(500, 5000)
+
+yondu = units * 0.13
+peter = yondu * 0.11
+crew = peter / number_pirates
+
+print(f"Number of crew: {number_pirates}")
+time.sleep(1.5)
+print(f"Number of total pirates: {number_pirates_w_other}")
+time.sleep(1.5)
+print(f"Units found: {units:.2f}")
+time.sleep(1.5)
+print(f"Yondu's share: {yondu:.2f}")
+time.sleep(1.5)
+print(f"Peter's share: {peter:.2f}")
+time.sleep(1.5)
+print(f"Crew's share: {crew:.2f}")
+time.sleep(1.5)
