bd226dfac595745b0b2a6ca29407e8a6acda7f0d
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2026-09-21 09:03:28 -0600

    changed

--- projects/crew_shares.py
+++ projects/crew_shares.py
@@ -23,8 +23,11 @@ number_pirates_w_other = number_pirates + 2
 units = random.randint(500, 5000)
 
 yondu = units * 0.13
-peter = yondu * 0.11
-crew = peter / number_pirates
+peter = (units - yondu) * 0.11
+crew = (units - yondu - peter) / number_pirates_w_other
+
+yondu_final = yondu + crew
+peter_final = peter + crew
 
 print(f"Number of crew: {number_pirates}")
 time.sleep(1.5)
@@ -32,9 +35,9 @@ print(f"Number of total pirates: {number_pirates_w_other}")
 time.sleep(1.5)
 print(f"Units found: {units:.2f}")
 time.sleep(1.5)
-print(f"Yondu's share: {yondu:.2f}")
+print(f"Yondu's share: {yondu_final:.2f}")
 time.sleep(1.5)
-print(f"Peter's share: {peter:.2f}")
+print(f"Peter's share: {peter_final:.2f}")
 time.sleep(1.5)
 for name in name_pirates:
     print(f"{name}'s share: {crew:.2f}")
