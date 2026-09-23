3b67d3d3d9f24d361aef8ea4d1c0f8a9119a7461
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2026-09-23 09:46:40 -0600

    done idk

--- notes/booleans.py
+++ notes/booleans.py
@@ -1,2 +1,13 @@
 # AE | P1 | Booleans
 
+win = False
+
+admin = True
+# not a boolean unless the first letter is capitalized, True and False
+
+age = 35
+
+if 18 < age: # <- boolean equation, always will be True or False
+    print("You are an adult")
+
+print(bool(age)) # <- everything is True execpt if its empty
--- projects/user_sigin.py
+++ projects/user_sigin.py
@@ -1,10 +1,16 @@
 # AE | P1 | User Sign In
 
+import time
+
+user = []
+
 signin = input("Is this your first sign in [YES / NO]? ").lower().strip()
 
 if signin == "yes":
-    username = input("Enter first username: ")
-    password = input("Enter first password: ")
+    username = input("Enter username: ")
+    password = input("Enter password: ")
 else:
     username = input("Enter username: ")
     password = input("Enter password: ")
+
+tries = 0
